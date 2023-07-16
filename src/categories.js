d7cd97fc7aec264590b7b9e238747291bb19e9a6
Vitaliy <vitohatv@gmail.com>
2023-07-16 21:43:53 +0600

    Добавил categories.js категории для выпадающего списка

---
+++ src/categories.js
@@ -0,0 +1,6 @@
+export const categories =  [
+    {title: 'Famous people', id: 'famous'},
+    {title: 'Saying', id: 'saying'},
+    {title: 'Humour', id: 'humour'},
+    {title: 'Motivation', id: 'motivation'}
+];
